8bddd3365089c858263c92af230675b99f628886
zhou-yg <zhou.y.g890@gmail.com>
2022-07-16 15:11:18 +0800

    feat: upgrade ts template

--- ts/hooks/todo.ts
+++ ts/hooks/todo.ts
@@ -1,43 +1,12 @@
 import {
-  inputCompute,
   state,
-  model,
-  inputComputeInServer,
-  computed
 } from 'tarat-core'
-
-function newTodoItem(description: string): { status: 'done' | 'undone', description: string } {
-  return {
-    status: 'undone',
-    description,
-  }
-}
 
 export default function todo () {
 
-  const items = model<{ id?: number, status: 'done' | 'undone', description: string }[]>('todoItem', () => ({
-    where: {
-      status: 'undone'
-    }
-  }), { immediate: true })
-
-  const undoneItems = computed(() => {
-    const arr = items()
-    if (arr) {
-      return arr.filter(item => item.status === 'undone')
-    }
-    return []
-  })
-
-  const createTodoItem = inputComputeInServer(async (description: string) => {
-    items(d => {
-      d.push(newTodoItem(description))
-    })
-  })
+  const s = state(0)
 
   return {
-    items,
-    createTodoItem,
-    undoneItems,
+    s
   }
 }
--- ts/views/todos.tsx
+++ ts/views/todos.tsx
@@ -1,73 +1,11 @@
 import { useTarat } from 'tarat-connect'
 import React, { useState } from 'react'
-import todoHook from '../hooks/todo.js'
-import './todos.less'
-
-const TodoItem = (props) => {
-  const checked = props.status === 'done'
-  return (
-    <div className="todoItem">
-      <input className='status' type="checkbox" checked={checked} onChange={() => {
-        props.onStatus(checked ? 'undone' : 'done')
-      }} />
-      <div className={"text " + (checked ? 'done' : '')}>
-        {props.description}
-      </div>
-      <div className="remove" onClick={props.onRemove}>
-        X
-      </div>
-    </div>
-  )
-}
 
 const Todos = () => {
 
-  const todo = useTarat(todoHook)
-  
-  const [inputText, setInputText] = useState('')
-
-  function keydownCreate (e) {
-    if (e.keyCode === 13 || e.key === 'Enter') {
-      todo.createTodoItem(e.target.value)
-      setInputText('')
-    }
-  }
-  
-
-  const undoneItemsNum = todo.undoneItems()?.length
-
   return (
-    <div className="home">
-      <div className='newItem'>
-        <input
-          onKeyDown={keydownCreate}
-          onChange={e => setInputText(e.target.value)}
-          value={inputText}
-          placeholder="Enter a new task" />
-      </div>
-      {todo.items()?.map((item, i) => {
-        return <TodoItem
-          onStatus={(s) => {
-            todo.items(d => {
-              d[i].status = s
-            })
-          }}
-          onRemove={() => {
-            todo.items(d => {
-              d.splice(i, 1)
-            })            
-          }}
-          key={item.id}
-          description={item.description}
-          status={item.status} />
-      })}
-      {undoneItemsNum !== undefined ? (
-        <div className='footer'>
-          <span className='leftItems'>
-            {undoneItemsNum} item left
-          </span>
-        </div>
-      ) : ''}
+    <div>
+      todos
     </div>
   )
 }
